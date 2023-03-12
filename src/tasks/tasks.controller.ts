f4eb951c68c3ae9873634f842549c1052244766b
bouba-coder <boubacaryatera1@gmail.com>
2023-03-12 21:48:19 +0100

    adding crud in grpc

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,8 +1,17 @@
 import { Controller } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { GrpcMethod, RpcException } from '@nestjs/microservices';
-import { CreateTaskRequest, Task } from 'stubs/task/v1alpha/task';
+import {
+  CreateTaskRequest,
+  ListTasksRequest,
+  ListTasksResponse,
+  Task,
+  GetTaskRequest,
+  UpdateTaskRequest,
+  DeleteTaskRequest
+} from 'stubs/task/v1alpha/task';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 import { status } from '@grpc/grpc-js';
 
 @Controller()
@@ -19,6 +28,13 @@ export class TasksController {
       return { ...task, dueDate: task.dueDate.toISOString() } as any;
     } catch (error) {
       console.log({ error });
+      if(error?.code === 'P2009'){
+        throw new RpcException({
+          code: status.INVALID_ARGUMENT,
+          message: 'Invalid argument(s)',
+        });
+      }
+      
       if (error?.code === 'P2002') {
         throw new RpcException({
           code: status.INVALID_ARGUMENT,
@@ -29,4 +45,98 @@ export class TasksController {
       throw new RpcException(error);
     }
   }
+
+  @GrpcMethod('TaskService')
+  async GetAllTasks(request: ListTasksRequest): Promise<ListTasksResponse> {
+    const { pageSize, pageToken } = request;
+    const {tasks, nextPageToken} = await this.tasksService.findAllTasks(pageSize, parseInt(pageToken));
+
+    return {
+      task: tasks.map((task) => {
+        return {
+          ...task,
+          dueDate: task.dueDate.toISOString(),
+        };
+      }),
+      nextPageToken,
+    };
+  }
+
+  @GrpcMethod('TaskService')
+  async GetTask(request: GetTaskRequest): Promise<Task> {
+    try {
+      const task = await this.tasksService.findOneByName(request.name);
+      if (!task) {
+        throw new RpcException({
+          code: status.NOT_FOUND,
+          message: `"${request.name}" not found`,
+        });
+      }
+      return {
+        ...task,
+        dueDate: task.dueDate.toISOString(),
+      };
+    } catch (error) {
+      if(error?.code === 'P2002'){
+        throw new RpcException({
+          code: status.NOT_FOUND,
+          message: request.name + ' not found',
+        });
+      }
+      throw new RpcException(error);
+    }
+  }
+
+  @GrpcMethod('TaskService')
+  async UpdateTask(request: UpdateTaskRequest): Promise<Task> {
+    try {
+      if(!request.task){
+        throw new RpcException({
+          code: status.INVALID_ARGUMENT,
+          message: 'Task is required',
+        });
+      }
+      const task = await this.tasksService.updateTask(request.task.id, new UpdateTaskDto(request.task));      
+      return {
+        ...task,
+        dueDate: task.dueDate.toISOString(),
+      };
+    } catch (error) {
+      if(error?.code === 'P2025'){
+        throw new RpcException({
+          code: status.NOT_FOUND,
+          message: request.task.id + ' not found',
+        });
+      }
+      throw new RpcException(error);
+    }
+  }
+
+  @GrpcMethod('TaskService')
+  async DeleteTask(request: DeleteTaskRequest): Promise<Task> {
+    try {
+      const task = await this.tasksService.removeTaskByName(request.name);
+      if (!task) {
+        throw new RpcException({
+          code: status.NOT_FOUND,
+          message: `"${request.name}" not found`,
+        });
+      }
+      return {
+        ...task,
+        dueDate: task.dueDate.toISOString(),
+      };
+    } catch (error) {
+      if(error?.code === 'P2025'){
+        throw new RpcException({
+          code: status.NOT_FOUND,
+          message: request.name + ' not found',
+        });
+      }
+      throw new RpcException(error);
+    }
+  }
+
 }
+  
+
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -9,6 +10,40 @@ export class TasksService {
   async create(createTaskDto: CreateTaskDto) {
     const task = await this.prisma.task.create({ data: createTaskDto });
     return task;
+  }
+
+  async findAllTasks(tasksPerPage: number, pageNumber: number) {
+    if (isNaN(pageNumber)) throw new Error('Page number must be a number');
+    if (isNaN(tasksPerPage)) throw new Error('Tasks per page must be a number');
+
+    const skip = tasksPerPage * (pageNumber - 1);
+    const tasks = await this.prisma.task.findMany({
+      skip,
+      take: tasksPerPage,
+    });
+    return {
+      tasks,
+      nextPageToken: tasks.length <= tasksPerPage ? '' : (pageNumber + 1).toString(),
+    };
+  }
+
+  findOneById(id: number) {
+    return this.prisma.task.findUnique({ where: { id } });
+  }
+
+  findOneByName(name: string) {
+    return this.prisma.task.findUnique({ where: { name } });
+  }
+
+  updateTask(id: number, updateTaskDto: UpdateTaskDto) {
+    return this.prisma.task.update({
+      where: { id },
+      data: updateTaskDto,
+    });
+  }
+
+  removeTaskByName(name: string) {
+    return this.prisma.task.delete({ where: { name } });
   }
 
   // findAll() {
