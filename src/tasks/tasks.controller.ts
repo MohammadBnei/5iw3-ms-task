a2de5b618ab09170b57d89ac506f57a0dac8634c
Adam Sellin <adamsellin1914@gmail.com>
2023-03-13 00:14:08 +0100

    implementation fonction CRUD in gRPC

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,8 +1,16 @@
 import { Controller } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { GrpcMethod, RpcException } from '@nestjs/microservices';
-import { CreateTaskRequest, Task } from 'stubs/task/v1alpha/task';
+import {
+  CreateTaskRequest,
+  Task,
+  DeleteTaskRequest,
+  ListTasksRequest,
+  ListTasksResponse,
+  UpdateTaskRequest,
+} from 'stubs/task/v1alpha/task';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 import { status } from '@grpc/grpc-js';
 
 @Controller()
@@ -26,6 +34,72 @@ export class TasksController {
         });
       }
 
+      throw new RpcException(error);
+    }
+  }
+
+  @GrpcMethod('TaskService')
+  async ListTasks(request: ListTasksRequest): Promise<ListTasksResponse> {
+    try {
+      const tasks = await this.tasksService.findAll(
+        request.pageSize,
+        request.pageToken,
+      );
+
+      return {
+        tasks: tasks.map((task) => ({
+          ...task,
+          dueDate: task.dueDate.toISOString(),
+        })),
+      } as any;
+    } catch (error) {
+      console.log({ error });
+      throw new RpcException(error);
+    }
+  }
+
+  @GrpcMethod('TaskService')
+  async UpdateTask(request: UpdateTaskRequest): Promise<Task> {
+    try {
+      const task = await this.tasksService.update(
+        request.task.name,
+        new UpdateTaskDto(request.task),
+      );
+
+      return { ...task, dueDate: task.dueDate.toISOString() } as any;
+    } catch (error) {
+      console.log({ error });
+      if (error?.code === 'P2002') {
+        throw new RpcException({
+          code: status.INVALID_ARGUMENT,
+          message: request.task.name + ' is already taken',
+        });
+      }
+
+      throw new RpcException(error);
+    }
+  }
+
+  @GrpcMethod('TaskService')
+  async DeleteTask(request: DeleteTaskRequest): Promise<Task> {
+    try {
+      const task = await this.tasksService.remove(request.name);
+
+      return { ...task, dueDate: task.dueDate.toISOString() } as any;
+    } catch (error) {
+      console.log({ error });
+      throw new RpcException(error);
+    }
+  }
+
+  @GrpcMethod('TaskService')
+  async GetTask(request: DeleteTaskRequest): Promise<Task> {
+    try {
+      const task = await this.tasksService.findOne(request.name);
+
+      return { ...task, dueDate: task.dueDate.toISOString() } as any;
+    } catch (error) {
+      console.log({ error });
       throw new RpcException(error);
     }
   }
