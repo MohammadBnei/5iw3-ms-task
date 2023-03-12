21cf574d0bbcbfc0dd519b32e538a16f5d859dd3
nikola panic <npanic@myges.fr>
2023-03-12 22:05:37 +0100

    cc1

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,7 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTaskDto } from './create-task.dto';
+import { Task } from 'stubs/task/v1alpha/task';
 
 export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+    constructor(task: Task) {
+        super(task);
+        this.done = task.done;
+    }
+
     name: string;
     dueDate: Date;
+    done: boolean;
 }
