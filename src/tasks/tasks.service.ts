6b034e46e20de14431ce1d842da68820f5198b23
Naderi Farid <geekwid@gmail.com>
2023-03-14 12:48:31 +0100

    feature task crud and docker-compose db

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,40 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
 export class TasksService {
   constructor(private prisma: PrismaService) {}
 
-  async create(createTaskDto: CreateTaskDto) {
-    const task = await this.prisma.task.create({ data: createTaskDto });
-    return task;
+  create(createTaskDto: CreateTaskDto) {
+    return this.prisma.task.create({ data: createTaskDto });
   }
 
-  // findAll() {
-  //   return this.taskRepository.findAll();
-  // }
+  findAll() {
+    return this.prisma.task.findMany();
+  }
 
-  // findOne(id: number) {
-  //   return this.taskRepository.findOne(id);
-  // }
+  findOne(id: number) {
+    return this.prisma.task.findUnique({ where: { id } });
+  }
 
-  // async update(id: number, updateTaskDto: UpdateTaskDto) {
-  //   let task = await this.taskRepository.findOne(id);
-  //   task = await this.taskRepository.assign(task, updateTaskDto);
-  //   await this.taskRepository.flush();
-  //   return task;
-  // }
+  update(id: number, updateTaskDto: UpdateTaskDto) {
+    return this.prisma.task.update({
+      where: { id },
+      data: updateTaskDto,
+    });
+  }
 
-  // async changeStatus(id: number) {
-  //   const task = await this.taskRepository.findOne(id);
-  //   task.done = !task.done;
-  //   await this.taskRepository.flush();
-  //   return task;
-  // }
+  changeStatus(id: number) {
+    return this.prisma.task.findUnique({ where: { id } }).then((task) => {
+      return this.prisma.task.update({
+        where: { id },
+        data: { done: !task.done },
+      });
+    });
+  }
 
-  // async remove(id: number) {
-  //   const task = await this.taskRepository.findOne(id);
-  //   return this.taskRepository.removeAndFlush(task);
-  // }
+  remove(id: number) {
+    return this.prisma.task.delete({ where: { id } });
+  }
 }
