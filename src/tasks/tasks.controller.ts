b84801b70d90ac25d440bb66bffac52b1674e7ae
n-panic <77328885+n-panic@users.noreply.github.com>
2023-03-14 10:24:40 +0100

    ajout nom

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,3 +1,5 @@
+// PANIC Nikola 5IW3
+
 import { Controller } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { GrpcMethod, RpcException } from '@nestjs/microservices';
