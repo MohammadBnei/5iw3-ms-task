a6ede74d972cad83fe06a8657ee8f7053d093eb2
julian-saleix <julian.saleix@gmail.com>
2023-03-14 10:26:56 +0100

    Adding name in tasks.controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,3 +1,4 @@
+//Julian Saleix 5IW3
 import { Controller } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { GrpcMethod, RpcException } from '@nestjs/microservices';
